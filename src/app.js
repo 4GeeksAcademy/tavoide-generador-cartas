f06e8e87082aeb58b45bf7612d70d3b3acee25fe
Tavoide <gustavobenitez116@gmail.com>
2024-11-29 22:18:18 +0000

    realice la funcion de las cartas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// window.onload = function() {
+//   //write your code here
+// };
+let palos = ["♠️", "♥️", "♦️", "♣️"];
+let valores = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let generarCarta = () => {
+  let palo = palos[Math.floor(Math.random() * palos.length)];
+  let valor = valores[Math.floor(Math.random() * valores.length)];
+  let figura = ` ${palo}`;
+  let numero = `${valor} ${palo}`;
+  document.getElementById("figura").textContent = figura;
+  document.getElementById("numero").textContent = numero;
 };
+generarCarta();
